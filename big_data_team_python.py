434bc6188024276358dd2dbb7f305ad08e2f07bd
Bango1020 <117309639+Bango1020@users.noreply.github.com>
2023-08-10 19:59:08 +0900

    Update big_data_team_python.py

--- big_data_team_python.py
+++ big_data_team_python.py
@@ -162,9 +162,9 @@ st.subheader('Top5 비교')
 def top5(col_name, top=5):
     my_agg = (df_simple.groupby(col_name)['충전량'].sum()).reset_index().sort_values('충전량', ascending=False).head(top)
     my_agg[col_name] = my_agg[col_name].astype('str')
-    fig = plt.figure(figsize=(15,10))
+    fig = plt.figure(figsize=(3,2))
     ax = sns.barplot(x='충전량', y=col_name, data=my_agg)
-    #ax.bar_label(ax.containers[0], label_type='center', color='white')
+    ax.bar_label(ax.containers[0], label_type='center', color='white')
     return fig
 
 col1, col2 = st.columns(2)
